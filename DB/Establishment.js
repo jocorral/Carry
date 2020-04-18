f632b4642e8b7d34a006461e4deae7fe060075d7
JoCorral <jocorral@opendeusto.es>
2020-04-18 15:35:18 +0200

    Change model to check if query works

--- DB/Establishment.js
+++ DB/Establishment.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const establishmentSchema = new mongoose.Schema({
     // _id : mongoose.Schema.Types.ObjectId,
-    name : String,
-    phoneNumber : Number,
-    location : String
+    name : {type:String},
+    phoneNumber : {type:Number},
+    location : {type:String}
 });
 
 module.exports = mongoose.model('Establishment', establishmentSchema);
