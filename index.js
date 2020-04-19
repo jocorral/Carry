1169cdb55bf88ea6fced5d70805db94f25d81061
JoCorral <jocorral@opendeusto.es>
2020-04-19 17:01:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -834,8 +834,8 @@ restService.post("/webhook", function (req, res) {
         //Encrypt data
         cvc_Encrypted = CryptoJS.AES.encrypt(JSON.stringify(cvc), KEY).toString();
         creditCardNum_Encrypted = CryptoJS.AES.encrypt(JSON.stringify(creditCardNum), cvc).toString();
-        /*expirationMonth_Encrypted = CryptoJS.AES.encrypt(JSON.stringify(expirationMonth), cvc).toString();
-        expirationYear_Encrypted = CryptoJS.AES.encrypt(JSON.stringify(expirationYear.substring(expirationYear.length - 2)), cvc).toString();*/
+        expirationMonth_Encrypted = CryptoJS.AES.encrypt(JSON.stringify(expirationMonth), cvc).toString();
+       /* expirationYear_Encrypted = CryptoJS.AES.encrypt(JSON.stringify(expirationYear.substring(expirationYear.length - 2)), cvc).toString();*/
       }
     }
 
