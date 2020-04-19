aaf9ca1f10f1b1095284ca5dc098f07c0edee04f
JoCorral <jocorral@opendeusto.es>
2020-04-19 16:38:25 +0200

    Remove encryption test

--- index.js
+++ index.js
@@ -832,10 +832,10 @@ restService.post("/webhook", function (req, res) {
 
       if (cvc) {
         //Encrypt data
-        cvc_Encrypted = CryptoJS.AES.encrypt(JSON.stringify(cvc), KEY).toString();
+        /*cvc_Encrypted = CryptoJS.AES.encrypt(JSON.stringify(cvc), KEY).toString();
         creditCardNum_Encrypted = CryptoJS.AES.encrypt(JSON.stringify(creditCardNum), cvc).toString();
         expirationYear_Encrypted = CryptoJS.AES.encrypt(JSON.stringify(expirationYear.substring(expirationYear.length - 2)), cvc).toString();
-        expirationMonth_Encrypted = CryptoJS.AES.encrypt(JSON.stringify(expirationMonth), cvc).toString();
+        expirationMonth_Encrypted = CryptoJS.AES.encrypt(JSON.stringify(expirationMonth), cvc).toString();*/
       }
     }
 
